7db1450829c3bb70d3812c21455299ab554c4020
Nic La <sakeofmaking@gmail.com>
2015-11-15 17:33:45 -0800

    Create heartbeat.h

---
+++ heartbeat.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * File Name:	heartbeat.h
+ * Program:		Project for Real-Time Embedded Systems Programming class
+ * Author:		Robert Weber
+ * Purpose:		Contains heartbeat definition
+ *
+ *  Date	Changed by:	Changes:
+ * -------	-----------	-------------------------------------------------------
+ * 24Apr02	R Weber		Original file.
+ ******************************************************************************/
+#if !defined(HEARTBEAT_H)		/* Prevents including this file multiple times */
+#define HEARTBEAT_H
+
+/* Function Prototypes */
+void heartbeat(void);
+
+#endif
