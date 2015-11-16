fa73909a068f63a8aca712b70a952a88fd79c6bc
Nic La <sakeofmaking@gmail.com>
2015-11-15 17:38:28 -0800

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * File Name:	serial.h
+ * Program:		Project for Real-Time Embedded Systems Programming class
+ * Author:		Robert Weber
+ * Purpose:		Header file for menu.c. 
+ *
+ *  Date	Changed by:	Changes:
+ * -------	-----------	-------------------------------------------------------
+ * 13Aug02	R Weber		Original file.
+ ******************************************************************************/
+#if !defined(MENU_H)		/* Prevents including this file multiple times */
+#define MENU_H
+
+/* Function Prototypes */
+void RunMenu(void);
+
+#endif /* MENU_H */
